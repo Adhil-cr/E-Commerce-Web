bf1d422b1e2804381636e7527d8fc22cdcd460b0
Adhil-cr <m.adhilcr7@gmail.com>
2025-11-23 17:10:07 +0530

    feat: initialize project with core apps and database models
    - Created base Django project structure
    - Added apps: customers, products,themes, orders
    - Implemented initial models for the E-Commerce system
    - Applied initial migrations successfully

--- config/products/models.py
+++ config/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Model for products
+class Product(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE,'live'),(DELETE,'delete'))
+
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    description = models.TextField(max_length=200)
+    product_image = models.ImageField(upload_to='media/')
+
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES,default=LIVE,db_index=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self)->str:
+        return self.title
+    
